97b43e08119f7734f2a891afb690172117694460
Daniel Smalley <desmalley@gmail.com>
2020-04-01 15:11:42 -0600

    I made the list convert to dictionary function more robust

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print("hello world")
+from My_Goog_Pack import goog_funcs as goog
+lol=goog.get_goog_data()
+topics=lol[0]
+
+username="Smalley"
+user_entry=goog.find_user(username,lol)
+
+
+
+if user_entry: #if user entry exists
+    user_dict=goog.convert_to_dict(user_entry,topics)
+else:
+    user_dict={}
+
+print(username + " Dictionary:")
+
+print(user_dict)
